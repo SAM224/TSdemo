4ccb7daf3ab714d8491b6772173053ab635e2dc0
Shady A. Abdulhamid <SAMCS224@hotmail.com>
2017-09-25 08:17:53 +0200

    Declare variables.

---
+++ 01_DeclaringVariables.ts
@@ -0,0 +1,4 @@
+var var1;           //  define variable with type any
+let var2;           //  define variable with type any
+let var3 = 10;      //  Initialization
+let var4: number;   //  defining datatype
---
+++ 02_types.ts
@@ -0,0 +1,15 @@
+let count = 5;
+
+let x;
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1,2,3];
+let f: any[] = [1, true, 'a', false];
+
+const width = 480; 
+
+enum Color { Red = 0, Green = 1, Blue = 2 };
+let backgroundColor = Color.Red;
