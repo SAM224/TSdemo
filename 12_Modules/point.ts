91fa2570e027353e19e045fb9ade6d02dff03b14
Shady A. Abdulhamid <SAMCS224@hotmail.com>
2017-09-26 23:12:23 +0200

    Modules.

--- 12_Modules/point.ts
+++ 12_Modules/point.ts
@@ -1,11 +1,8 @@
-class Point {
+export class Point {
     constructor(private x?: number, private y?: number) {}
-
+    
     draw() {
         console.log('x: '+ this.x+ ' y: '+ this.y);
     }
 
 }
-
-let point = new Point(1,2);   
-point.draw();
