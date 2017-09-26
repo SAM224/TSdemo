bd0358490a2ae8de0fb860c3fa78a882c84f1ca3
Shady A. Abdulhamid <SAMCS224@hotmail.com>
2017-09-26 23:44:47 +0200

    enhance  exersice.

--- Exercise/LikeButton.ts
+++ Exercise/LikeButton.ts
@@ -3,12 +3,14 @@ export class LikeButton{
         this.ShowResult();
     }
     Toggle(): void {
-        this.likesCount = (this.isLiked)? this.likesCount-1 : this.likesCount+1;
+        this.likesCount += (this.isLiked)? -1 : +1;
         this.isLiked = ! this.isLiked;
         this.ShowResult();
     }
 
     ShowResult(){ 
-        console.log('Total Number of likes:' + this.likesCount + ' Button status: '+ (this.isLiked?'liked': 'not liked'));
+        console.log('Total Number of likes:' + 
+        this.likesCount + ' Button status: '+ 
+        (this.isLiked?'liked': 'not liked'));
     }
 }
